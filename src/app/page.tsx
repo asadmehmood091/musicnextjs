19994e165e17062b15ce863ac12ce01db49406c2
Asad Mehmood <45494881+asadmehmood091@users.noreply.github.com>
2024-03-12 15:31:58 +0000

    navbar, navbar-menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+
 export default function Home(){
   return (
-    <div>
-      <h1>Music Learning Web App</h1>
-      <p>Welcome! Let's learn about music together.</p>
-    </div>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      {/* <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer /> */}
+    </main>
   );
 }
